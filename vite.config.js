43b5ea0f671a0c259b2659c947bfaecb701f8352
RavaszTamas <ravaszt88@gmail.com>
2023-04-19 16:26:53 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
+  css: {
+    postcss,
+  },
+  server: {
+    proxy: {
+      "/contamy-nation": {
+        target: "https://contamy-nation.com",
+        changeOrigin:true,
+        rewrite: (path) => path.replace(/^\/contamy-nation/,'')
+      },
+      "^/_next/.*": {
+        target: "https://contamy-nation.com",
+        changeOrigin:true,
+      },
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
